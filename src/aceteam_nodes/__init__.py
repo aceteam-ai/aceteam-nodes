1d9d38ab14eb6b745de4bc5ea055cabd6fb2f024
Justin Xu <justin@adanomad.com>
2026-06-25 20:35:00 +0000

    Remove comparison nodes
    They are moved to the next release of workflow-engine itself

--- src/aceteam_nodes/__init__.py
+++ src/aceteam_nodes/__init__.py
@@ -4,33 +4,15 @@ __version__ = "6.1.0"
 
 
 from .nodes import (
-    AndNode,
     APICallNode,
     BrowserFetchNode,
-    EqualNode,
-    GreaterThanEqualNode,
-    GreaterThanNode,
-    LessThanEqualNode,
-    LessThanNode,
     LLMNode,
-    NotEqualNode,
-    NotNode,
-    OrNode,
     XPathExtractNode,
 )
 
 __all__ = [
     "APICallNode",
-    "AndNode",
     "BrowserFetchNode",
-    "EqualNode",
-    "GreaterThanEqualNode",
-    "GreaterThanNode",
     "LLMNode",
-    "LessThanEqualNode",
-    "LessThanNode",
-    "NotEqualNode",
-    "NotNode",
-    "OrNode",
     "XPathExtractNode",
 ]
